a9a5f1246868a0ff7c0b5bb49d2e1e2e4bf61da4
Abhijit Mondal <abhijit.manpur@gmail.com>
2023-06-12 08:14:20 +0530

    pushing for toml

--- src/pinggy/pinggyConnection.py
+++ src/pinggy/pinggyConnection.py
@@ -5,6 +5,7 @@ import json
 import time
 import pinggy.fileno as fileno
 import queue
+import select
 
 HOST_KEY="SHA256:nFd5rfJMGuZXvfeRzJ/BtT3TfksAxTWMajcrHRcI7AM"
 
@@ -25,13 +26,19 @@ class Connection():
         self.connected = False
         self.forwardingStarted = False
         self.urls = []
-        self.rfd, wfd = None, None
-        self.acceptQueue = queue.Queue()
+        self.transportThread = None
+        self.listeningSocket = None
+        self.transportThreadLock = threading.Lock()
     
     def _connectSocket(self):
         sock = socket.socket(type = socket.SOCK_STREAM)
         sock.connect((self.server,self.port))
         return sock
+
+    def close(self):
+        if self.transport is not None:
+            self.transport.close()
+            self.transport = None
     
     def connect(self):
         self.lock.acquire()
@@ -92,64 +99,91 @@ class Connection():
         if "urls" in dt:
             self.urls = dt["urls"]
     
-    def _acceptChan(self, chan, fromAddr, toAddr):
-        print("New channel", chan, " from: ", fromAddr, " to: ", toAddr)
-        if chan is None:
-            print(self.transport.get_exception())
-            return
-        self.acceptQueue.put(chan)
-        self.wfd.set()
+    # def _acceptChan(self, chan, fromAddr, toAddr):
+    #     print("New channel", chan, " from: ", fromAddr, " to: ", toAddr)
+    #     if chan is None:
+    #         print(self.transport.get_exception())
+    #         return
+    #     self.acceptQueue.put(chan)
+    #     self.wfd.set()
+
+    def copy(self, sock1, sock2):
+        try:
+            while True:
+                dt = sock1.recv(2048)
+                # print(f"recv {len(dt)} from {sock1} to {sock2}")
+                if len(dt) == 0:
+                    break
+                sock2.sendall(dt)
+        finally:
+            # print(f"closing {sock1}, {sock2}")
+            sock1.close()
+            sock2.close()
+
+    def _acceptLoop(self, serverAddr):
+        # print(f"waiting for connection at {self.transport}")
+        while True:
+            chan = self.transport.accept()
+            if chan is None:
+                return False #the transport is closed
+                #do stuff
+            # print(f"chan: {chan} recved")
+            sock = socket.socket()
+            try:
+                sock.connect(serverAddr)
+                # print(f"chan: {chan} and sock: {sock} connected")
+                threading.Thread(target=self.copy, args=(sock, chan,)).start()
+                threading.Thread(target=self.copy, args=(chan, sock,)).start()
+            except:
+                sock.close()
+                chan.close()
+        return True
     
-    def startLisener(self):
-        print("Start listening")
+    def startForwarding(self, serverAddr):
+        # print("Start listening")
+        # print(f"forwarding to http://{serverAddr[0]}:{serverAddr[1]}")
         self.lock.acquire()
         if self.forwardingStarted:
             return
         try:
-            self.transport.request_port_forward("localhost:4000", 0, self._acceptChan)
+            self.transport.request_port_forward(f"{serverAddr[0]}:{serverAddr[1]}", 0)
             self.forwardingStarted = True
             self._fetchUrls()
+            self.transportThread = threading.Thread(target=self._acceptLoop, args=(serverAddr,))
+            self.transportThread.start()
+            # self.rfd, self.wfd = fileno.getInfoPipe()
+            # print(self.urls)
 
-            self.rfd, self.wfd = fileno.getInfoPipe()
-            print(self.urls)
         finally:
             self.lock.release()
 
-    def bind(self, address):
-        pass
-
-    def listen(self):
-        self.startLisener()
-
-    def _accept(self):
-        while True:
-            chan = self.transport.accept()
-            print("Accepted a connection: ", chan)
-            if chan is None:
-                print(self.transport.get_exception())
-                continue
-            self.acceptQueue.put(chan)
-            self.wfd.set()
-
-    def fileno(self):
-        print("Requested for file no")
-        self.lock.acquire()
+    def wait(self):
+        self.transportThreadLock.acquire()
         try:
-            if self.rfd is None:
-                self.rfd, self.wfd = fileno.getInfoPipe()
-                # x = threading.Thread(target=self._accept, args=())
-                # x.start()
-            return self.rfd.fileno()
+            if self.transportThread is not None:
+                self.transportThread.join()
+                self.transportThread = None
+        except KeyboardInterrupt:
+            pass
         finally:
-            self.lock.release()
-
-    def accept(self):
-        print("call accept")
+            self.transportThreadLock.release()
+    
+    def startListening(self, port=0, host="localhost"):
         self.lock.acquire()
+        if self.listeningSocket is not None:
+            return self.listeningSocket
         try:
-            chan = self.acceptQueue.get()
-            self.rfd.clear()
-            return chan
+            sock = socket.create_server((host, port))
+            self.listeningSocket = sock
+        except Exception as e:
+            print(e)
         finally:
             self.lock.release()
+        
+        if self.listeningSocket is not None:
+            self.startForwarding(self.listeningSocket.getsockname())
+        
+        return self.listeningSocket
 
+    def getUrls(self):
+        return self.urls
