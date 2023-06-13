16a27f78c336efc1dd9f896984b811d8b6afcd54
Abhijit Mondal <abhijit.manpur@gmail.com>
2023-06-13 08:13:23 +0530

    Changed the from pinggy.http to pinggy.file

--- src/pinggy/serve.py
+++ src/pinggy/serve.py
@@ -1,12 +1,14 @@
 from pinggy import Connection
 import http.server as httpServer
 import threading
+import os
+import argparse
 
 def serveOnThread(httpServer):
     httpServer.serve_forever()
 
-def serveFile():
-    conn = Connection()
+def serveFile(token):
+    conn = Connection(token=token)
     conn.connect()
     serverAddress = ('', 0) # Use any available port
     hServer = httpServer.HTTPServer(serverAddress, httpServer.SimpleHTTPRequestHandler)
@@ -28,4 +30,10 @@ def serveFile():
         conn.close()
 
 if __name__ == "__main__":
-    serveFile()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-D", "--directory", default="./", help="local directory to serve")
+    parser.add_argument("-t", "--token", default="", help="user token. one can be find from pinggo.io")
+    args = parser.parse_args()
+    os.chdir(args.directory)
+    serveFile(args.token)
